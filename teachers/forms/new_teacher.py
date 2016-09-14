2ed56838d43021b91ea39d4ac6e29fbd0fade4d9
Moritz Krog <moritzkrog@gmail.com>
2016-09-14 18:26:00 +0200

    Cleaned-up teacher section

--- teachers/forms/new_teacher.py
+++ teachers/forms/new_teacher.py
@@ -8,3 +8,11 @@ class NewTeacherForm(forms.ModelForm):
     class Meta:
         model = Teacher
         fields = ['name']
+
+    def __init__(self, *args, **kwargs):
+        super(NewTeacherForm, self).__init__(*args, **kwargs)
+        self.set_widget_css_class('form-control')
+
+    def set_widget_css_class(self, class_name):
+        for field in self.fields.keys():
+            self.fields[field].widget.attrs['class'] = class_name
