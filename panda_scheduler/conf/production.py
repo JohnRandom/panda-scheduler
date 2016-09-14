de9ca298bdb1545ce8565287a24b8e3cc2329bb6
Moritz Krog <moritzkrog@gmail.com>
2016-09-14 15:21:19 +0200

    Fixed a typo

--- panda_scheduler/conf/production.py
+++ panda_scheduler/conf/production.py
@@ -27,7 +27,7 @@ ALLOWED_HOSTS = ['*']
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
-Extra places for collectstatic to find static files.
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
